57c03e1f9c403dcdf27566fc7d55874219b6fa87
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-07-14 17:39:49 -0300

    Requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,37 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    result = []
+    for file in instance.queue:
+        txt_file = txt_importer(file)
+        occurrences = []
+        for index, line in enumerate(txt_file):
+            if word in line.lower():
+                occurrences.append({"linha": index + 1})
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file,
+                "ocorrencias": occurrences
+            })
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    result = []
+    for file in instance.queue:
+        txt_file = txt_importer(file)
+        occurrences = []
+        for index, line in enumerate(txt_file):
+            if word in line.lower():
+                occurrences.append({"linha": index + 1, "conteudo": line})
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file,
+                "ocorrencias": occurrences
+            })
+    return result
