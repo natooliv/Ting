f56a2538b15d368e1055139f9dc1de28148ca25d
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-07-14 15:39:06 -0300

    Requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,4 +1,11 @@
 import sys
-
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+        return None
+    try:
+        with open(path_file, 'r', encoding="utf_8") as document:
+            return document.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
